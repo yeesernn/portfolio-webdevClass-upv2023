f79d9fe3fd8810f1fb6dec367cfed18cabd7aece
eddyWSL <57491959+yeesernn@users.noreply.github.com>
2023-05-09 10:03:46 +0200

    Finish up series1.html added interactive js auto hide menu bar also

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,14 @@ menuButton.addEventListener("click", toggleMenu)
 function toggleMenu() {
     menuTransition.classList.toggle("showMenu");
 }
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector("#navBar").style.top = "0";
+  } else {
+    document.querySelector("#navBar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+} 
