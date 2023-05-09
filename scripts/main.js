d708bbdf2fd4287da3d47b1e37b803467e4c9bf3
eddyWSL <57491959+yeesernn@users.noreply.github.com>
2023-05-09 10:54:48 +0200

    Import Headroom.js and applied to series1, 2, 3

--- scripts/main.js
+++ scripts/main.js
@@ -6,24 +6,3 @@ menuButton.addEventListener("click", toggleMenu)
 function toggleMenu() {
     menuTransition.classList.toggle("showMenu");
 }
-
-
-/*
-var prevScrollPos = window.pageYOffset;
-
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollPos > currentScrollPos) {
-    document.querySelector("#navBar").style.top = "0";
-  } else {
-    document.querySelector("#navBar").style.top = "-100px";
-  }
-  prevScrollPos = currentScrollPos;
-} 
-*/
-
-/*!
- * headroom.js v0.12.0 - Give your page some headroom. Hide your header until you need it
- * Copyright (c) 2020 Nick Williams - http://wicky.nillia.ms/headroom.js
- * License: MIT
- */
